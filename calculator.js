7c969bdce2d1bba733f8f00dd4389b7e8b55aa22
abdulrehman <alrehmanbilal@gmail.com>
2019-10-31 15:07:05 +0500

    javascript file added

---
+++ calculator.js
@@ -0,0 +1,18 @@
+var a = Number(prompt("First number"));
+var b = Number(prompt("Second number"));
+var c = prompt("Type Operation (+,-,*,/,%)");
+if(c === "+"){
+    alert(a+b);
+}
+else if(c === "-"){
+    alert(a-b);
+}
+else if(c === "*"){
+    alert(a*b);
+}
+else if(c === "/"){
+    alert(a/b);
+}
+else if(c === "%"){
+    alert(a%b);
+}
